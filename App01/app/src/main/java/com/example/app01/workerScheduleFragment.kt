fe962462c024ed56873d5edc449998f6476e088c
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-10-28 19:38:23 +0900

    Adapting list to RecyclerView

--- App01/app/src/main/java/com/example/app01/workerScheduleFragment.kt
+++ App01/app/src/main/java/com/example/app01/workerScheduleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import com.example.app01.databinding.FragmentWorkerScheduleBinding
 
@@ -16,6 +17,10 @@ class workerScheduleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        for (i in 0..5) {
+            var newOne : itemAnnouncement = itemAnnouncement(i,i,"Announcement Test " + i,"Description Test " + i,false)
+            dataObject.listAnnouncement.add(newOne)
+        }
         val binding : FragmentWorkerScheduleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_worker_schedule, container, false)
         binding.layoutCalculation.setOnClickListener {
             it.findNavController().navigate(R.id.action_workerScheduleFragment_to_workerCalculationFragment)
@@ -26,7 +31,11 @@ class workerScheduleFragment : Fragment() {
         binding.layoutMyPage.setOnClickListener {
             it.findNavController().navigate(R.id.action_workerScheduleFragment_to_workerMypageFragment)
         }
+
         adapter = itemAnnouncementAdapter(dataObject.listAnnouncement, requireContext())
+        binding.listAnnouncement.layoutManager = LinearLayoutManager(context).also { it.orientation = LinearLayoutManager.HORIZONTAL }
+        binding.listAnnouncement.adapter = adapter
+
         var snapHelper : PagerSnapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(binding.listAnnouncement)
         return binding.root
